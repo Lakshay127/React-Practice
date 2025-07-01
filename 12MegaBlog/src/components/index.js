5f3df4c8f89efd5727472669deb206f99a0a57e2
Lakshay Aggarwal <aggarwallakshay370@gmail.com>
2025-07-01 23:49:55 +0530

    Mega Blog Project Completed

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './container/Container'
+import Logo  from './Logo'
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from './RTE'
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from './Button/Button'
+import PostForm from './post-form/PostForm'
+import Protected from './AuthLayout'
+import Input from './Input'
+import PostCard from './PostCard'
+import Select from './Select'
+import AuthLayout from './AuthLayout'
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostCard,
+    Protected,
+    PostForm,
+    Select,
+    Input,
+    AuthLayout
+
+}
